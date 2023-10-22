ce659d4e7cd19512624e3e42f4ce54838e996365
Denys L <lyepustin@tutanota.com>
2023-10-22 22:04:21 +0200

    langchain summarization: 1 step

--- fundamentals/LangchainSummarizer.py
+++ fundamentals/LangchainSummarizer.py
@@ -16,6 +16,11 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chat_models import ChatOpenAI
+
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -40,8 +45,38 @@ def summarization_map_reduce(data_path):
     chain.run(texts[:4])
 
 
+def summarization_stuff(data_path):
+    llm = OpenAI(temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    loader = TextLoader(data_path, encoding="utf-8")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=16000, chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    # Define prompt
+    prompt_template = """Write a concise summary of the following:
+    "{text}"
+    CONCISE SUMMARY:"""
+    prompt = PromptTemplate.from_template(prompt_template)
+
+    # Define LLM chain
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k", openai_api_key=os.getenv("OPENAI_API_KEY"))
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+    # Define StuffDocumentsChain
+    stuff_chain = StuffDocumentsChain(
+        llm_chain=llm_chain, document_variable_name="text", verbose=True
+    )
+
+    print(texts[0])
+    print(stuff_chain.run([texts[0]]))
+
+
+
 
 if __name__ == '__main__':
     path_parent = pathlib.Path(__file__).parent.parent.resolve()
     data_path = f"{path_parent}/data/quijote.txt"
-    summarization_map_reduce(data_path)
+    # summarization_map_reduce(data_path)
+    summarization_stuff(data_path)
+    # tutorial: https://python.langchain.com/docs/use_cases/summarization
