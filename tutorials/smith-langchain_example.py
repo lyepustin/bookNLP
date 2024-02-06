0acd2c7722400fed229b9591307dc01caab265e3
Denys L <lyepustin@tutanota.com>
2024-02-06 22:48:37 +0100

    LangSmith introduction

---
+++ tutorials/smith-langchain_example.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Set environment variables
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_SMITH_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "default"
+
+# The below examples use the OpenAI API, so you will need
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI()
+response = llm.invoke("Hello, world!")
+print(response)
